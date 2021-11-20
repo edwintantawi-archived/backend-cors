967d1f4970d6ac929f3cf0aba607f60b16345001
edwintantawi <edwintantawi@gmail.com>
2021-11-20 11:13:07 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500', 'https://frontend-cors.netlify.app/'],
     credentials: true,
     allowedHeaders: 'Set-Cookie',
   })
