93161e00b605433d5a037349d4c66ae024af7f20
edwintantawi <edwintantawi@gmail.com>
2021-11-20 13:45:55 +0700

    no cookies

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,17 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (_, res) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.cookie(
-    'token',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
-    {
-      sameSite: 'none',
-      secure: true,
-      httpOnly: true,
-      expires: new Date(Date.now() + 9999999),
-    }
-  );
+  // res.header('Access-Control-Allow-Credentials', true);
+  // res.cookie(
+  //   'token',
+  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  //   {
+  //     sameSite: 'none',
+  //     secure: true,
+  //     httpOnly: true,
+  //     expires: new Date(Date.now() + 9999999),
+  //   }
+  // );
   return res.json({ message: 'token cookie added' });
 });
 
