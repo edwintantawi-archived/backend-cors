5931e7117af7e0a5b58740cfd0f5548abd0275b1
edwintantawi <edwintantawi@gmail.com>
2021-11-28 17:25:13 +0700

    update 3

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,17 +10,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (_, res) => {
-  res.cookie(
-    'token',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
-    {
-      sameSite: 'none',
-      secure: true,
-      httpOnly: true,
-      path: '/api/auth',
-      expires: new Date(Date.now() + 9999999),
-    }
-  );
+  const dummyToken = `tkn-${+new Date()}`;
+  res.cookie('token', dummyToken, {
+    sameSite: 'none',
+    secure: true,
+    httpOnly: true,
+    path: '/api/auth',
+    expires: new Date(Date.now() + 9999999),
+  });
   return res.json({ message: 'token cookie added' });
 });
 
