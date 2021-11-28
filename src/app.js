fdf462a32984b8f877ba3b0f20ea1f71c3c8796d
edwintantawi <edwintantawi@gmail.com>
2021-11-28 21:41:45 +0700

    updated

--- src/app.js
+++ src/app.js
@@ -1,26 +1,55 @@
-const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'https://frontend-cors.netlify.app',
-    credentials: true,
+    origin: process.env.ORIGIN || 'http://127.0.0.1:5500', // [allowed origin]
+    credentials: true, // [accept cookie and Authorization]
   })
 );
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(cookieParser()); // [allowed to handle cookie]
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
+// [act like auth feature to validate accessToken from cookie]
+app.get('/api/auth', (req, res) => {
+  const { accessToken } = req.cookies;
+  if (!accessToken)
+    return res.json({
+      status: 'fail',
+      message: 'accessToken not found',
+    });
 
-routes(app);
+  return res.json({
+    status: 'success',
+    message: 'authenticated',
+    accessToken,
+  });
+});
+
+// [act like login feature and return auth token by cookie]
+app.get('/api/auth/login', (_, res) => {
+  const accessToken = `tkn-${+new Date()}`;
+
+  const expires = new Date(Date.now() + 9999999);
+
+  res.cookie('accessToken', accessToken, {
+    sameSite: 'none', // [allow cross-origin] [require secure]
+    secure: true, // [https only] [use in production]
+    httpOnly: true, // [http] [https] [not accessable by javascript]
+    expires, // [expires]
+  });
+
+  return res.json({
+    status: 'success',
+    message: 'accessToken cookie added',
+    accessToken,
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
