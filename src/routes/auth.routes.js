8dcb0832019ae61b4f3a165a271d4e1fdf5ab81b
edwintantawi <edwintantawi@gmail.com>
2021-11-28 16:08:57 +0700

    update

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,13 +14,12 @@ router.get('/login', (_, res) => {
     'token',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
     {
-      sameSite: 'none',
       secure: true,
       httpOnly: true,
       expires: new Date(Date.now() + 9999999),
     }
   );
-  res.json({ message: 'token cookie added' });
+  return res.json({ message: 'token cookie added' });
 });
 
 module.exports = router;
