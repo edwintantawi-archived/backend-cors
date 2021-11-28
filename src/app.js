7fb438abcd0426188cd74f410b8a31ef964190cb
edwintantawi <edwintantawi@gmail.com>
2021-11-28 15:11:02 +0700

    preflightContinue

--- src/app.js
+++ src/app.js
@@ -14,13 +14,8 @@ const whitelist = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    preflightContinue: true,
+    origin: whitelist,
     credentials: true,
   })
 );
